e28072af055263bb6047d3f5cd953afdc2e97fcb
Dylan Kainth <dylan.kainth@kcl.ac.uk>
2026-04-25 17:35:21 +0200

    YEETED THE PROJECT

--- harvest/provider-ui/app.js
+++
@@ -1,8 +0,0 @@
-/** @typedef {import('pear-interface')} */ /* global Pear */
-
-// This file initializes the Pear desktop app
-// It handles communication between the backend (index.js) and the HTML window
-
-Pear.updates(() => Pear.reload())
-
-// The HTML will connect via Pear.pipe to receive state updates
